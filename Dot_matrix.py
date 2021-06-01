7d3e679ded8df472b4d992d30fbcd6e6536ebfca
Remon Isaac Gamil <48691269+RemonIsaac@users.noreply.github.com>
2021-06-02 01:14:29 +0200

    Fixed a bug, now the algorithm works correctly

--- Dot_matrix.py
+++ Dot_matrix.py
@@ -1,27 +1,28 @@
 from tabulate import tabulate
 
-def dot_matrix(a, b, size, step, thresh):
-    dot_arr = [[' ']* (len(a)+1) for i in range(len(b) + 1)] 
-    for i in range(1, len(a) + 1):
-        dot_arr[0][i] = a[i-1]
+def dot_matrix(seq1, seq2, size, step, thresh):
+    dot_arr = [[' ']* (len(seq1)+1) for i in range(len(seq2) + 1)] 
+    for i in range(1, len(seq1) + 1):
+        dot_arr[0][i] = seq1[i-1]
 
-    for i in range(1, len(b) + 1):
-        dot_arr[i][0] = b[i-1]
+    for i in range(1, len(seq2) + 1):
+        dot_arr[i][0] = seq2[i-1]
 
     def thresh_check(a, b): 
         count = 0
-        for i,j in zip(a,b):
+        for j,i in zip(a,b):
             if i == j:
                 count += 1
         if count >= thresh: 
             return True
 
-    for i in range(0, len(b) - size + 1, step):
-        for j in range(0, len(a) - size + 1, step):
-            if thresh_check(a[i*step: i*step + size], b[j*step: j*step + size]): 
-                dot_arr[j*step + size//2 + 1][i*step + size//2 + 1] = 'x'
+    for i in range(0, len(seq1) - size + 1, step):
+        for j in range(0, len(seq2) - size + 1, step):
+            if thresh_check(seq1[i: i + size], seq2[j: j + size]): 
+                dot_arr[j + size//2 + 1][i + size//2 + 1] = 'x'
 
-    print(tabulate(dot_arr, tablefmt="github"))    
-
-
+    print(tabulate(dot_arr, tablefmt="github"))   
+ 
+if __name__ == "__main__":
+    dot_matrix(seq1 = "ACCTTGTCCTCTTTGCCC" , seq2 = "ACGTTGACCTGTAACCTC", size = 9, step=3, thresh = 4)
 
